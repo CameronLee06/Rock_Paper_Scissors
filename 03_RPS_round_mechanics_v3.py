6eb876f64d74c4b96a998b430b7b67bead0e660d
CameronLee06 <99371789+CameronLee06@users.noreply.github.com>
2022-03-25 12:18:45 +1300

    Add files via upload

--- 03_RPS_round_mechanics_v3.py
+++ 03_RPS_round_mechanics_v3.py
@@ -37,18 +37,19 @@ while end_game =="no":
     # Rounds Heading 
     print()
     if rounds == "":
-        heading = "Infinite Mode: Round {}".format(rounds_played + 1)
-        print(heading)
-        choose = input("{} or 'xxx' to end: ".format(choose_instruction))
-        if choose == "xxx":
-            break
-    else:
+        heading = "Infinite Mode: \ Round {}".format(rounds_played + 1)
+    
+    else: 
         heading = "Round {} of {}".format(rounds_played + 1, rounds)
-        print(heading)
-        choose = input(choose_instruction)
-        if rounds_played == rounds - 1:
-            end_game = "yes"
 
+
+    print(heading)
+    choose = input ("{} or 'xxx' to end: ").format(choose_instruction)
+
+
+    # End game if exit code is typed
+    if choose == "xxx":
+        break
 
     # rest of loop / game
     print("You chose {}".format(choose))
