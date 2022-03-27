88f264627ceac6eebcaed082478905b040f2ded7
CameronLee06 <99371789+CameronLee06@users.noreply.github.com>
2022-03-28 10:54:57 +1300

    Add files via upload

--- 01_RPS_Base_Component_v02.py
+++ 01_RPS_Base_Component_v02.py
@@ -21,8 +21,34 @@ def check_rounds():
         return response
 
 
+def choice_checker(question, valid_list, error):
+
+    error = "Please choose from rock / paper / scissors (or xxx to quit)"
+
+    valid = False
+    while not valid:
+
+        # Ask user for choice (and put choice in lowercase)
+        response = input(question).lower()
+
+
+        # Iterates through list and if response is an item
+        # in the list (or the first letter of an item), the 
+        # full item name is returned
+
+        for item in valid_list:
+            if response == item[0] or response == item:
+                return item
+
+        # output error if item not in list
+        print(error)
+        print()
+
 
 # Main routine goes here...
+
+yes_no_list = ["yes", "no"]
+rps_list = ["rock", "paper", "scissors", "xxx"]
 
 rounds_played = 0 
 choose_instruction = "Please choose rock (r) , paper / (p) or scissors (s) or 'xxx' to end"
@@ -39,17 +65,18 @@ while end_game =="no":
     if rounds == "":
         heading = "Infinite Mode: Round {}".format(rounds_played + 1)
         print(heading)
-
+        if rounds == rounds_played == rounds + 1:
+            break
     else: 
         heading = "Round {} of {}".format(rounds_played + 1, rounds)
 
 
     print(heading)
-    choose = input(choose_instruction)
+    choose = choice_checker(choose_instruction, rps_list, "Please enter R / P / S")
 
 
     # End game if exit code is typed
-    if choose == "xxx" or rounds_played == rounds - 1:
+    if choose == rounds_played == rounds - 1:
         break
 
     # rest of loop / game
